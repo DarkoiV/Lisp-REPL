a539ba062293681fd2f31a21aae6112b5ab5b4f7
Maksymilian <makkob997@gmail.com>
2024-01-16 18:22:49 +0100

    Can now parse basic sexpr

--- Main.hs
+++ Main.hs
@@ -9,8 +9,11 @@ data Symbol = Add
             | Symbol String
 
 str2symbol :: String -> Symbol 
-str2symbol "Add" = Add
-str2symbol str   = Symbol str
+str2symbol "+" = Add
+str2symbol "-" = Sub
+str2symbol "*" = Mul
+str2symbol "/" = Div
+str2symbol str = Symbol str
 
 data SExpr = SExpr Symbol [Val]
 data Val = Nil | Expr SExpr | Number Float
@@ -25,12 +28,38 @@ fromNumber (Just n)  = Number n
 
 eval :: SExpr -> Val
 eval (SExpr Add vals) = fromNumber . (fmap sum) . sequence $ fmap toNumber vals  
+eval (SExpr Sub vals) 
+  | length vals == 2 = fromNumber $ fmap (foldr (-) 0) . sequence $ fmap toNumber vals
+  | otherwise        = Nil
+eval (SExpr Mul vals) = fromNumber . (fmap product) . sequence $ fmap toNumber vals  
+eval (SExpr Div vals) 
+  | length vals == 2 = fromNumber $ fmap (foldr (/) 0) . sequence $ fmap toNumber vals
+  | otherwise        = Nil
+eval _ = undefined
 
 symbolP :: Parser Symbol
 symbolP = fmap str2symbol $ stringP "Add" 
       <|> stringP "Sub"
       <|> stringP "Mul"
       <|> stringP "Div"
+
+valP :: Parser Val
+valP = sexpr <|> number <|> nil
+  where
+    sexpr  = fmap Expr sexprP 
+    number = fmap Number (numberP) 
+    nil    = pure Nil
+
+sexprP :: Parser SExpr 
+sexprP = do
+  charP '('
+  ignorewsP
+  symbol <- symbolP 
+  vals <- some valP 
+  ignorewsP
+  charP ')'
+  return (SExpr symbol vals)
+  
 
 main :: IO ()
 main = return ()
