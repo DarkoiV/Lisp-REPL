63bb42b96e3cbf6fdbb56b0d247b14ba0603b84a
Maksymilian <makkob997@gmail.com>
2024-01-19 15:08:09 +0100

    Sub and Div will apply to all it's numeric values

--- Sexpr.hs
+++ Sexpr.hs
@@ -46,13 +46,9 @@ fromNumber (Just n)  = Number n
 
 eval :: SExpr -> Val
 eval (SExpr Add vals) = fromNumber . (fmap sum) . sequence $ fmap toNumber vals  
-eval (SExpr Sub vals) 
-  | length vals == 2 = fromNumber $ fmap (foldr (-) 0) . sequence $ fmap toNumber vals
-  | otherwise        = Nil
+eval (SExpr Sub vals) = fromNumber $ fmap (foldl1 (-)) . sequence $ fmap toNumber vals
 eval (SExpr Mul vals) = fromNumber . (fmap product) . sequence $ fmap toNumber vals  
-eval (SExpr Div vals) 
-  | length vals == 2 = fromNumber $ fmap (foldr (/) 0) . sequence $ fmap toNumber vals
-  | otherwise        = Nil
+eval (SExpr Div vals) = fromNumber $ fmap (foldl1 (/)) . sequence $ fmap toNumber vals
 eval _ = undefined
 
 symbolP :: Parser Symbol
